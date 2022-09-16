72cc0b0d9e8204419744992c0b845e26ca2ebd23
Tomas <tomas@codi.gy>
2022-09-16 21:33:57 +0300

    360 image

---
+++ public/js/360_image.js
@@ -0,0 +1,13 @@
+let PanoViewer = require('@egjs/view360')
+const panoViewer = new PanoViewer.PanoViewer(
+    document.getElementById("pano"),
+    {
+        image: "https://nftrelicart.com/assets/site-images/360_panorama-test-optimize-4k.jpg",
+        height: 800,
+        useZoom: false,
+    }
+);
+
+window.addEventListener("resize", function() {
+    panoViewer.updateViewportDimensions();
+});
