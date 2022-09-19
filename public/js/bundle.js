44eac12cb44290176f3503e8a8ab07f108b27486
Tomas <tomas@codi.gy>
2022-09-19 19:04:16 +0300

    .

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -22585,7 +22585,7 @@ let PanoViewer = require('@egjs/view360')
 const panoViewer = new PanoViewer.PanoViewer(
     document.getElementById("pano"),
     {
-        image: "https://nftrelicart.com/assets/site-images/360_panorama-test-optimize-4k.jpg",
+        image: "https://nftrelicart.com/assets/site-images/360_panorama-optimize-4k.jpg",
         height: 800,
         useZoom: false,
     }
