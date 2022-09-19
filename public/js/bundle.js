714b3f06ebececb0eef2f32d2021241fad52d560
Tomas <tomas@codi.gy>
2022-09-19 19:10:44 +0300

    .

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -22588,6 +22588,7 @@ const panoViewer = new PanoViewer.PanoViewer(
         image: "https://nftrelicart.com/assets/site-images/360_panorama-optimize-4k.jpg",
         height: 800,
         useZoom: false,
+        touchDirection: PanoViewer.PanoViewer.TOUCH_DIRECTION.YAW
     }
 );
 
