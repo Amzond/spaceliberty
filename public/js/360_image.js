d8179396b177a6a2a4de4b09dd3e05007d936f77
Tomas <tomas@codi.gy>
2022-09-20 10:25:48 +0300

    uploaded new 360 image

--- public/js/360_image.js
+++ public/js/360_image.js
@@ -2,7 +2,7 @@ let PanoViewer = require('@egjs/view360')
 const panoViewer = new PanoViewer.PanoViewer(
     document.getElementById("pano"),
     {
-        image: "https://nftrelicart.com/assets/site-images/360_panorama-optimize-4k.jpg",
+        image: "https://nftrelicart.com/assets/site-images/peter_house_800.jpg",
         height: 800,
         useZoom: false,
         touchDirection: PanoViewer.PanoViewer.TOUCH_DIRECTION.YAW
